40907307e6414ecd0c03aa3e184ad5e873471f5d
vitdolinek <dolinekvit@gmail.com>
2026-09-11 12:37:35 +0200

    add: counter test suites, `user-event` testing library

--- src/components/counter.spec.tsx
+++ src/components/counter.spec.tsx
@@ -1,13 +1,55 @@
-import { render, screen } from '@testing-library/react'
-import { Counter } from './counter'
-import { describe, expect, test } from 'vitest'
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { Counter } from "./counter";
+import { describe, expect, test } from "vitest";
 
-describe('Counter', () => {
-  test('should render interface', () => {
-    render(<Counter />)
+describe("Counter", () => {
+  test("should render interface", () => {
+    render(<Counter />);
 
-    expect(screen.getByRole('button', { name: '+' })).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: '-' })).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument()
-  })
-})
+    expect(screen.getByText(/current count\:/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "+" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "-" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /reset/i })).toBeInTheDocument();
+  });
+
+  test("should add to counter on button press", async () => {
+    const user = userEvent.setup();
+
+    render(<Counter />);
+
+    expect(screen.getByText(/current count\: 0/i)).toBeInTheDocument();
+
+    await user.click(screen.getByRole("button", { name: "+" }));
+
+    expect(screen.getByText(/current count\: 1/i)).toBeInTheDocument();
+  });
+
+  test("should remove to counter on button press", async () => {
+    const user = userEvent.setup();
+
+    render(<Counter />);
+
+    expect(screen.getByText(/current count\: 0/i)).toBeInTheDocument();
+
+    await user.click(screen.getByRole("button", { name: "-" }));
+
+    expect(screen.getByText(/current count\: \-1/i)).toBeInTheDocument();
+  });
+
+  test("should reset the counter", async () => {
+    const user = userEvent.setup();
+
+    render(<Counter />);
+
+    expect(screen.getByText(/current count\: 0/i)).toBeInTheDocument();
+
+    await user.click(screen.getByRole("button", { name: "+" }));
+
+    expect(screen.getByText(/current count\: 1/i)).toBeInTheDocument();
+
+    await user.click(screen.getByRole("button", { name: /reset/i }));
+
+    expect(screen.getByText(/current count\: 0/i)).toBeInTheDocument();
+  });
+});
